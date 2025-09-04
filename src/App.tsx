4436f58bbad2abefd061b125ee495bb6b7f55af7
Max Rieck <maxermusrieck@gmail.com>
2025-09-04 07:56:03 -0700

    added edit and delete actions to products

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App:React.FC = () => {
     <AuthProvider>
       <RegistrationForm />
       <AddProductForm />
-      <UserList />
+      {/* <UserList /> */}
       <ProductList />
     </AuthProvider>
   )
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../firebase';
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, doc, updateDoc, deleteDoc } from 'firebase/firestore'
 
 interface Product {
     id?: string;
@@ -12,7 +12,23 @@ interface Product {
  
 const ProductList:React.FC = () => {
 
-    const [products, setProducts] = useState<Product[]>([])
+    const [products, setProducts] = useState<Product[]>([]);
+    const [newName, setNewName] = useState<string>('');
+    const [newDescription, setNewDescription] = useState<string>('');
+    const [newPrice, setNewPrice] = useState<number>(0)
+
+
+    const updateProduct = async (productId:string, updatedData: string | number) => {
+        const userDoc = doc(db, 'products', productId);
+        await updateDoc(userDoc, updatedData);
+        alert('Product updated!');
+
+    }
+
+    const deleteProduct = async (productId:string) => {
+        await deleteDoc(doc(db, 'products', productId));
+        alert('Product deleted!');
+    }
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -27,13 +43,38 @@ const ProductList:React.FC = () => {
     }, [])
 
   return (
-    
+
     <div>
         <h2>Product List</h2>
         {products.map((products) => (
-            <div key={products.id}>
-                <p>Name: {products.name}</p>
-                <p>Price: {products.price}</p>
+            <div key={products.id} 
+                style={{border: 'solid 1px', margin: "1rem"}}>
+                <div>
+                    <p>Name: {products.name}</p>
+                    <p>Price: {products.price}</p>
+                </div>
+                <input
+                    onChange={(e) => setNewName(e.target.value)}
+                    type="string"
+                    placeholder="Enter new name:"/>
+                <button onClick={() => updateProduct(products.id, { name: newName })}>
+                    Update Name
+                </button>
+                <input
+                    onChange={(e) => setNewDescription(e.target.value)}
+                    type="string"
+                    placeholder="Enter new description:"/>
+                <button onClick={() => updateProduct(products.id, { description: newDescription })}>
+                    Update Description
+                </button>
+                <input
+                    onChange={(e) => setNewPrice(Number(e.target.value))}
+                    type="number"
+                    placeholder="Enter new price:"/>
+                <button onClick={() => updateProduct(products.id, { price: newPrice })}>
+                    Update Price
+                </button>
+                <button onClick={() => deleteProduct(products.id)}>Delete Product</button>
             </div>
         ))}
     </div>
