1b833eb83f33760a8ccf618fb00523dac4ebfad7
Max Rieck <maxermusrieck@gmail.com>
2025-09-26 15:40:41 -0700

    applied timeout to home banner

--- src/pages/Hompage.tsx
+++ src/pages/Hompage.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PageLayout from './PageLayout';
+import { Link } from 'react-router-dom'
 
 
    
 const Hompage:React.FC = () => {
+
+    const [visible, setVisible] = useState<boolean>(false)
+
+     useEffect(() => {
+    const timer = setTimeout(() => setVisible(true), 100);
+    return () => clearTimeout(timer);
+  }, []);
 
 
 return (
 
     <PageLayout pageType='default'>
         <h1
-            className='homeBanner'
-        >Welcome to PowerOn Gaming</h1>
+            className={`homeBanner${visible ? ' visible' : ''}`}
+        >Welcome to <strong>PowerOn</strong> Gaming</h1>
         <img src="/images/banner2.png" alt="" />
 
-        <button className='defaultBtn mx-8 mt-3'>Show Now</button>
+        <Link to='products' 
+            className='defaultBtn mt-4 mx-8 inline-block'> 
+            Show Now 
+        </Link>
 
     </PageLayout>
 
